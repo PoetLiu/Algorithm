9e5b44d6a48955c8cd2dbf6bae7b348ec2ba5cee
Albert Liu <457566268@qq.com>
2017-08-16 11:19:38 +0800

    Create 231.c

---
+++ 231/231.c
@@ -0,0 +1,10 @@
+bool isPowerOfTwo(int n) {
+    if (n <= 0)
+        return false;
+    while (n > 1) {
+        if (n&1)
+            return false;
+        n   = n >> 1;
+    }
+    return true;
+}
